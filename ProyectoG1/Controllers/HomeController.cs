6ffdcac814374d111544df86300b9aed4aea8c24
JosueAVD27 <ajvelasquez4@espe.edu.ec>
2023-01-18 22:26:32 -0500

    Version 5

--- ProyectoG1/Controllers/HomeController.cs
+++ ProyectoG1/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ProyectoG1.Models;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +29,23 @@ namespace ProyectoG1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        //Agregar una imagen
+        public ActionResult Imagen()
+        {
+            var imag = Session["id"];
+            registro_calificacionesEntities db = new registro_calificacionesEntities();
+            usuarios usuariomodel = db.usuarios.Find(imag);
+            byte[] byteImage = usuariomodel.fotoUsuario;
+            MemoryStream memoryStream = new MemoryStream(byteImage);
+            Image imagen = Image.FromStream(memoryStream);
+            memoryStream = new MemoryStream();
+            imagen.Save(memoryStream, ImageFormat.Png);
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "image/jpg");
         }
     }
 }
